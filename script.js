4ab9bb0575886d1f2a98023d947f1535498f5d11
M45Hasan <mmhasan045@gmail.com>
2022-10-05 23:02:00 +0600

    CreateElementJS

---
+++ script.js
@@ -0,0 +1,29 @@
+/*let change = document.querySelector(".one");
+let buon = document.querySelector(".width");
+
+buon.addEventListener("click", function () {
+  if (change.className == "one two") {
+    change.className = "one";
+  } else {
+    change.className = "one two";
+  }
+});*/
+
+let ull = document.querySelector(".ull");
+let newLi = document.createElement("li");
+
+newLi.innerHTML = "No.7 from JS";
+
+//ull.before(newLi);
+//ull.after(newLi);
+
+let x = ull.children;
+
+console.log(x);
+
+for (let i = 0; i <= x.length; i++) {}
+console.log(x[2].nextElementSibling.before(newLi));
+
+//ull.append(newLi);
+
+//ull.lastElementChild.before(newLi); */
